e07edaec7bbf4b202562582178e458e0b64d3c43
Otto <ottoengelhardt18@gmail.com>
2024-11-05 16:04:49 -0300

    feat: add authentication with clerk

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,19 @@
-const Dashboard = () => {
-  return <h1>Dashboard page</h1>;
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const Dashboard = async () => {
+  const { userId } = await auth();
+  //SE NÃO TIVER LOGADO REDIRECIONAR PARA A PAGE DE LOGIN
+  if (!userId) {
+    redirect("/login");
+  }
+
+  return (
+    <div className="flex h-full items-center justify-center">
+      <UserButton showName />
+    </div>
+  );
 };
 
 export default Dashboard;
