826cb8ebe37a016d675fcb10363d6b4b29a92394
Otto <ottoengelhardt18@gmail.com>
2024-11-08 23:43:47 -0300

    feat: add summary cards

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,15 +1,41 @@
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import NavBar from "../_components/navbar";
+import SummaryCards from "./_components/summary-cards";
+import TimeSelect from "./_components/time-select";
+import { isMatch } from "date-fns";
 
-const Dashboard = async () => {
+interface HomeProps {
+  searchParams: {
+    month: string;
+  };
+}
+
+const Dashboard = async ({ searchParams: { month } }: HomeProps) => {
   const { userId } = await auth();
   //SE NÃO TIVER LOGADO REDIRECIONAR PARA A PAGE DE LOGIN
   if (!userId) {
     redirect("/login");
   }
 
-  return <NavBar />;
+  const monthIsInvalid = !month || !isMatch(month, "MM");
+
+  if (monthIsInvalid) {
+    redirect("?month=01");
+  }
+
+  return (
+    <>
+      <NavBar />
+      <div className="space-y-6 p-6">
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-bold">Dashboard</h1>
+          <TimeSelect />
+        </div>
+        <SummaryCards month={month} />
+      </div>
+    </>
+  );
 };
 
 export default Dashboard;
