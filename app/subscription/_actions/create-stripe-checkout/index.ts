f272fb12b65780ac9579d7f98758149fcef45b67
Otto <ottoengelhardt18@gmail.com>
2024-11-12 20:56:02 -0300

    feat: ensure user is redirected to stripe checkout when acquiring plan

---
+++ app/subscription/_actions/create-stripe-checkout/index.ts
@@ -0,0 +1,38 @@
+"use server";
+
+import { auth } from "@clerk/nextjs/server";
+import Stripe from "stripe";
+
+export const createStripeCheckout = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
+
+  if (!process.env.STRIPE_SECRET_KEY) {
+    throw new Error("Stripe Secret Key not found");
+  }
+
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+    apiVersion: "2024-10-28.acacia",
+  });
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    mode: "subscription",
+    success_url: "http://localhost:3000/dashboard",
+    cancel_url: "http://localhost:3000/dashboard",
+    subscription_data: {
+      metadata: {
+        clerk_user_id: userId,
+      },
+    },
+    line_items: [
+      {
+        price: process.env.STRIPE_PRO_PLAN_PRICE_ID,
+        quantity: 1,
+      },
+    ],
+  });
+  return { sessionId: session.id };
+};
