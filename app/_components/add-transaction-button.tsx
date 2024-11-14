36519f3026166c2d50991cc4fc784a4ca97880e4
Otto <ottoengelhardt18@gmail.com>
2024-11-13 23:43:21 -0300

    feat: ensure user can not create transaction on free plan if above limit

--- app/_components/add-transaction-button.tsx
+++ app/_components/add-transaction-button.tsx
@@ -4,19 +4,42 @@ import { ArrowDownUpIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import { useState } from "react";
 import UpsertTransactionDialog from "./upsert-transaction-dialog";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip";
 
-const AddTransactionButton = () => {
+interface AddTransactionButtonProps {
+  userCanAddTransaction?: boolean;
+}
+
+const AddTransactionButton = ({
+  userCanAddTransaction,
+}: AddTransactionButtonProps) => {
   const [dialogIsOpen, setDialogisOpen] = useState(false);
 
   return (
     <>
-      <Button
-        className="rounded-full font-bold"
-        onClick={() => setDialogisOpen(true)}
-      >
-        Adicionar transação
-        <ArrowDownUpIcon />
-      </Button>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button
+              className="rounded-full font-bold"
+              onClick={() => setDialogisOpen(true)}
+              disabled={!userCanAddTransaction}
+            >
+              Adicionar transação
+              <ArrowDownUpIcon />
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            {!userCanAddTransaction &&
+              "Você atingiu o limite de transações. Atualize o seu plano para criar transações ilimitadas."}
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
       <UpsertTransactionDialog
         isOpen={dialogIsOpen}
         setIsOpen={setDialogisOpen}
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -6,6 +6,7 @@ import NavBar from "../_components/navbar";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { ScrollArea } from "../_components/ui/scroll-area";
+import { canUserAddTransaction } from "../_data/can-user-add-transaction";
 
 const TransactionsPage = async () => {
   const { userId } = await auth();
@@ -18,7 +19,7 @@ const TransactionsPage = async () => {
       userId,
     },
   });
-
+  const userCanAddTransaction = await canUserAddTransaction();
   return (
     <>
       <NavBar />
@@ -26,7 +27,7 @@ const TransactionsPage = async () => {
         {/* TITULO E BOTÂO*/}
         <div className="flex w-full items-center justify-between">
           <h1 className="text-2xl font-bold">Transações</h1>
-          <AddTransactionButton />
+          <AddTransactionButton userCanAddTransaction={userCanAddTransaction} />
         </div>
         <ScrollArea>
           <DataTable
