15d5169b51274f2f467b2bfce3a9de984fa75973
Otto <ottoengelhardt18@gmail.com>
2024-12-18 22:01:04 -0300

    refactor: ensure subscription page is responsive

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,11 +1,11 @@
 import { auth, clerkClient } from "@clerk/nextjs/server";
-import NavBar from "../_components/navbar";
 import { redirect } from "next/navigation";
 import { Card, CardContent, CardHeader } from "../_components/ui/card";
 import { CheckIcon, XIcon } from "lucide-react";
 import AcquirePlanButton from "./_components/acquire-plan-button";
 import { Badge } from "../_components/ui/badge";
 import { getCurrentMonthTransactions } from "../_data/get-current-month-transactions";
+import Navbar from "../_components/navbar";
 
 const SubscriptionPage = async () => {
   const { userId } = await auth();
@@ -17,67 +17,74 @@ const SubscriptionPage = async () => {
   const hasProPlan = user.publicMetadata.subscriptionPlan == "pro";
   return (
     <>
-      <NavBar />
-      <div className="space-y-6 p-6">
-        <h1 className="text-2xl font-bold">Assinatura</h1>
+      <div className="overflow-y-auto">
+        <Navbar />
+        <div className="mx-auto max-w-[2012px] space-y-6 p-4 sm:p-6">
+          <h1 className="text-2xl font-bold">Assinatura</h1>
+          {/* CARDS */}
+          <div className="flex flex-col-reverse flex-wrap justify-center gap-6 sm:flex-row">
+            {/* FREE */}
+            <Card className="mx-auto w-72 sm:mx-0 sm:w-[450px]">
+              <CardHeader className="border-b border-solid py-8">
+                <h2 className="text-center text-2xl font-semibold">
+                  Plano Básico
+                </h2>
+                {/* PREÇO */}
+                <div className="flex items-center justify-center gap-3">
+                  <span className="text-4xl">R$</span>
+                  <span className="text-6xl font-semibold">0</span>
+                  <span className="text-2xl text-muted-foreground">mês</span>
+                </div>
+              </CardHeader>
+              <CardContent className="space-y-6 py-8">
+                <div className="flex items-center gap-2">
+                  <CheckIcon className="text-primary" />
+                  <p className="max-w-[20ch] sm:max-w-full">
+                    Apenas 10 transações por mês ({currentMonthTransactions}/10)
+                  </p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <XIcon className="text-danger" />
+                  <p>Relatórios de IA</p>
+                </div>
+              </CardContent>
+            </Card>
 
-        {/* CARDS */}
-        <div className="flex gap-6">
-          {/* FREE */}
-          <Card className="w-[450px]">
-            <CardHeader className="border-b border-solid py-8">
-              <h2 className="text-center text-2xl font-semibold">
-                Plano Básico
-              </h2>
-              {/* PREÇO */}
-              <div className="flex items-center justify-center gap-3">
-                <span className="text-4xl">R$</span>
-                <span className="text-6xl font-semibold">0</span>
-                <div className="text-2xl text-muted-foreground">/mês</div>
-              </div>
-            </CardHeader>
-            <CardContent className="space-y-6 py-8">
-              <div className="flex items-center gap-2">
-                <CheckIcon className="text-primary" />
-                <p>
-                  Apenas 10 transações por mês ({currentMonthTransactions}/10)
-                </p>
-              </div>
-              <div className="flex items-center gap-2">
-                <XIcon className="text-danger" />
-                <p>Relatórios de IA</p>
-              </div>
-            </CardContent>
-          </Card>
-
-          {/* PRO */}
-          <Card className="w-[450px]">
-            <CardHeader className="relative border-b border-solid py-8">
-              {hasProPlan && (
-                <Badge className="absolute left-4 top-11 bg-primary/10 text-primary">
-                  Ativo
-                </Badge>
-              )}
-              <h2 className="text-center text-2xl font-semibold">Plano Pro</h2>
-              {/* PREÇO */}
-              <div className="flex items-center justify-center gap-3">
-                <span className="text-4xl">R$</span>
-                <span className="text-6xl font-semibold">19</span>
-                <div className="text-2xl text-muted-foreground">/mês</div>
-              </div>
-            </CardHeader>
-            <CardContent className="space-y-6 py-8">
-              <div className="flex items-center gap-2">
-                <CheckIcon className="text-primary" />
-                <p>Transações ilimitadas</p>
-              </div>
-              <div className="flex items-center gap-2">
-                <CheckIcon className="text-primary" />
-                <p>Relatórios de IA</p>
-              </div>
-              <AcquirePlanButton />
-            </CardContent>
-          </Card>
+            {/* PRO */}
+            <Card className="mx-auto w-72 sm:mx-0 sm:w-[450px]">
+              <CardHeader className="relative mt-1 border-b border-solid py-8">
+                {hasProPlan ? (
+                  <Badge className="absolute left-2 top-2 bg-primary/10 text-sm text-primary sm:left-6 sm:top-10">
+                    Atual
+                  </Badge>
+                ) : (
+                  <Badge className="absolute left-2 top-2 bg-purple-600/10 text-sm text-purple-900 hover:bg-purple-500/10 sm:left-6 sm:top-10">
+                    Popular
+                  </Badge>
+                )}
+                <h2 className="text-center text-2xl font-semibold">
+                  Plano Pro
+                </h2>
+                {/* PREÇO */}
+                <div className="flex items-center justify-center gap-3">
+                  <span className="text-4xl">R$</span>
+                  <span className="text-6xl font-semibold">19</span>
+                  <span className="text-2xl text-muted-foreground">/mês</span>
+                </div>
+              </CardHeader>
+              <CardContent className="space-y-6 py-8">
+                <div className="flex items-center gap-2">
+                  <CheckIcon className="text-primary" />
+                  <p>Transações ilimitadas</p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <CheckIcon className="text-primary" />
+                  <p>Relatórios de IA</p>
+                </div>
+                <AcquirePlanButton />
+              </CardContent>
+            </Card>
+          </div>
         </div>
       </div>
     </>
