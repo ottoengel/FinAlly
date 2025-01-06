85b7327e4f1a1155bb65d93cf7b9bfc392854d17
Otto <ottoengelhardt18@gmail.com>
2025-01-06 08:50:22 -0300

    refactor: changed the text

--- app/(home)/sections/header.tsx
+++ app/(home)/sections/header.tsx
@@ -1,8 +1,11 @@
+import { auth } from "@clerk/nextjs/server";
 import { ArrowRight, MenuIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
-const Header = () => {
+const Header = async () => {
+  const { userId } = await auth();
+
   return (
     <header className="sticky top-0 z-20 backdrop-blur-sm">
       <div className="flex items-center justify-center bg-black py-3 text-sm text-white">
@@ -22,14 +25,19 @@ const Header = () => {
             />
             <MenuIcon className="h-5 w-5 items-center md:hidden" />
             <nav className="hidden items-center gap-6 md:flex">
-              <a href="#about">About</a>
+              <a href="#about">Sobre</a>
               <a href="#features">Features</a>
               <a href="#price">Preço</a>
-              <a href="#">Preço</a>
               <Link href="/login">
-                <button className="inline-flex items-center rounded-lg bg-black px-4 py-2 font-medium tracking-tight text-white">
-                  Get for free
-                </button>
+                {!userId ? (
+                  <button className="inline-flex items-center rounded-lg bg-black px-4 py-2 font-medium tracking-tight text-white">
+                    Comece Agora
+                  </button>
+                ) : (
+                  <button className="inline-flex items-center rounded-lg bg-black px-4 py-2 font-medium tracking-tight text-white">
+                    Dashboard
+                  </button>
+                )}
               </Link>
             </nav>
           </div>
